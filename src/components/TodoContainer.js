2be715414545f4a342b5626b9125e742b969de28
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-07-04 16:19:20 -0300

    added functionality to input field and styles

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,6 +1,8 @@
 import React from "react"
 import TodosList from './TodosList'
 import Header from "./Header";
+import InputTodo from "./InputTodo";
+import { v4 as uuidv4 } from 'uuid';
 
 class TodoContainer extends React.Component {
   state = {
@@ -35,11 +37,37 @@ class TodoContainer extends React.Component {
       }),
     }))
   };
+  delTodo = id => {
+    this.setState({
+      todos: [
+        ...this.state.todos.filter(todo => {
+          return todo.id !== id;
+        })
+      ]
+    })
+  }
+  addTodoItem = title => {
+    const newTodo = {
+      id: uuidv4(),
+      title: title,
+      completed: false
+    };
+    this.setState({
+      todos: [...this.state.todos, newTodo]
+    });
+  };
   render() {
     return (
-      <div>
-        <Header />
-        <TodosList todos={this.state.todos} handleChangeProps={this.handleChange} />
+      <div className="container">
+        <div className="inner">
+          <Header />
+          <InputTodo addTodoProps={this.addTodoItem} />
+          <TodosList
+            todos={this.state.todos}
+            handleChangeProps={this.handleChange}
+            deleteTodoProps={this.delTodo}
+          />
+        </div>
       </div>
     );
   }
