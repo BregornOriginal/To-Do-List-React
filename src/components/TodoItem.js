241a4836ccee89ad2f2e2166fdc32cb2efddc8bf
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-07-04 17:18:19 -0300

    added lifecycle and localStorage using an API

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -15,6 +15,9 @@ export default class TodoItem extends React.Component {
       this.setState({ editing: false })
     }
   }
+  componentWillUnmount() {
+    console.log("Cleaning up...")
+  }
   render() {
     const completedStyle = {
       fontStyle: "italic",
