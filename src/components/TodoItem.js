c98e3546c67282b2a352115f3ce85bc46b19f33f
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-07-04 15:26:47 -0300

    Added header and checkbox onchange event

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 export default class TodoItem extends React.Component {
-  render () {
-    return <li>{this.props.todo.title}</li>
+  render() {
+    return (
+      <li>
+        <input
+          type="checkbox"
+          checked={this.props.todo.completed}
+          onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+        />
+        {this.props.todo.title}
+      </li>
+    )
   }
 }
